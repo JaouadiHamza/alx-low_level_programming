ee6cee97d6c5882799c9880e344a262f1b479e42
Hamza Jaouadi <jawham7@gmail.com>
2023-02-16 15:24:26 +0100

    task8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE 
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19"\n)
+	return (1);
+}
